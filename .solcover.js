a1156fbeeebff58ead897b50aa4abb076065b03d
Sebastian Kim <sydneyitguy@gmail.com>
2023-12-06 16:29:00 +0900

    Update packages, README

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,10 @@
 module.exports = {
-  skipFiles: ['lib/ERC20Initializable.sol', 'lib/ERC1155Initializable.sol', 'mock/TestToken.sol', 'mock/TestMultiToken.sol', 'mock/TaxToken.sol']
+  skipFiles: [
+    "MCV1_Wrapper.sol",
+    "lib/ERC20Initializable.sol",
+    "lib/ERC1155Initializable.sol",
+    "mock/TestToken.sol",
+    "mock/TestMultiToken.sol",
+    "mock/TaxToken.sol"
+  ]
 };
